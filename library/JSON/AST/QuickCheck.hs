98726f4d4fe748a71d3a26b72c38f5525542f320
Nikita Volkov <nikita.y.volkov@mail.ru>
2016-01-28 19:04:10 +0300

    Init

---
+++ library/JSON/AST/QuickCheck.hs
@@ -0,0 +1,24 @@
+module JSON.AST.QuickCheck where
+
+import Prelude
+import JSON.AST
+import Test.QuickCheck
+import Test.QuickCheck.Instances
+
+
+instance Arbitrary JSON where
+  arbitrary =
+    oneof [null, bool, number, string, array, object]
+    where
+      null =
+        pure Null
+      bool =
+        fmap Bool arbitrary
+      number =
+        fmap Number arbitrary
+      string =
+        fmap String arbitrary
+      array =
+        fmap Array arbitrary
+      object =
+        fmap Object arbitrary
